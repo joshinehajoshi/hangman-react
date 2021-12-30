368489352eb9c8304fb04069e30ed88083737764
Neha Joshi <nehajoshigkp@gmail.com>
2021-12-30 15:46:56 +0530

    popup comp added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './Components/Header';
 import Figure from './Components/Figure';
 import WrongLetters from './Components/WrongLetters';
 import Word from './Components/Word';
+import Popup from './Components/Popup';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Figure />
       <WrongLetters />
       <Word />
+      <Popup />
       </div>
     </>
   );
